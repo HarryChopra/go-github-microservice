56374d308e830aa65d4bb4460ad2ce7b42c6f4ed
Harry Chopra <harrychopra@gmail.com>
2021-08-07 09:20:16 +0100

    Rest client impl for Post requests

---
+++ src/api/clients/rest_client/rest_client.go
@@ -0,0 +1,41 @@
+package restclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// RestClient allows mocking of Do method of type http.Client
+type RestClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+var (
+	Client RestClient
+)
+
+func init() {
+	Client = &http.Client{}
+}
+
+// POST makes an HTTP Post request and returns an HTTP response
+func Post(headers http.Header, url string, data interface{}) (*http.Response, error) {
+
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal: %v", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(js))
+
+	if err != nil {
+		return nil, fmt.Errorf("could not create a new http request, %v", err)
+	}
+
+	request.Header = headers
+
+	return Client.Do(request)
+}
