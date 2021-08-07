7ed11a721232c04586d2ee37a870f89c9d09111a
Harry Chopra <harrychopra@gmail.com>
2021-08-07 09:23:30 +0100

    Unit test Rest client

---
+++ src/api/clients/rest_client/rest_client_test.go
@@ -0,0 +1,40 @@
+package restclient
+
+import (
+	"io"
+	"math"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/harrychopra/go-github-microservice/src/api/utils/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	Client = &mocks.MockClientImpl{}
+	os.Exit(m.Run())
+}
+
+func TestPost_marshalError(t *testing.T) {
+	response, err := Post(nil, "", math.NaN)
+
+	assert.Nil(t, response)
+	if assert.NotNil(t, err) {
+		assert.Contains(t, err.Error(), "could not marshal")
+	}
+}
+
+func TestPost_requestCreationError(t *testing.T) {
+
+	body := io.NopCloser(strings.NewReader(`{"id":123}`))
+	mocks.SetDoFunc(body, http.StatusCreated, nil)
+
+	response, err := Post(nil, "", nil)
+
+	assert.Nil(t, err)
+	if assert.NotNil(t, response) {
+		assert.Equal(t, http.StatusCreated, response.StatusCode)
+	}
+}
